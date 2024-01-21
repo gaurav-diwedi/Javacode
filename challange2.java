831612c717286e927e354bb4d1c3de4601e6f264
gaurav-diwedi <gauravdiwedi0101@gmail.com>
2024-01-21 08:15:13 +0530

    Type Conversion

---
+++ challange2.java
@@ -0,0 +1,14 @@
+
+
+public class challange2 {
+    public static void main(String[] args) {
+    //implicit type comversion    
+        float myFloat= 5;
+        System.out.println(myFloat);
+
+    //explicit type conversion (passing bigger values into smaller)
+        int myInt = (int)5.45d;
+        System.out.println(myInt);  
+
+    }
+}
