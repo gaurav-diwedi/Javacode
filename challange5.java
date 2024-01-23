5ab69e39282fbf9f0a881a8e0cbd215cdbcd0e46
gaurav-diwedi <gauravdiwedi0101@gmail.com>
2024-01-23 15:14:25 +0530

    Airtmetic Operator

---
+++ challange5.java
@@ -0,0 +1,20 @@
+public class challange5 {
+    public static void main(String[] args) {
+        int x=70;
+        int y=35;
+        System.out.println(x);
+        System.out.println(y);
+        int sum=x+y;
+        System.out.println(sum);
+        int sub =x-y;
+        System.out.println(sub);
+        int mult =x*y;
+        System.out.println(mult);
+        int div=x/y;;
+        System.out.println(div);
+        int rem=x%y;;
+        System.out.println(rem);
+
+       
+    }
+}
