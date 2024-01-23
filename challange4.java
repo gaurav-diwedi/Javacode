583bdb41d82149c21d1473927f37ff8dfa849a78
gaurav-diwedi <gauravdiwedi0101@gmail.com>
2024-01-23 15:04:32 +0530

    Pre & Post unary operator

--- challange4.java
+++ challange4.java
@@ -1,9 +1,15 @@
 public class challange4 {
     public static void main(String[] args) {
         int x=7;
-        int y= -x;
-        int z = ++y;
-    
-        System.out.println(z);
+        System.out.println(x);
+        ++x;
+        System.out.println(x);
+        --x;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+       
     }
 }
